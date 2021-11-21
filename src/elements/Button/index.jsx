55bdc7cd0be684294ee508435e1dc66f7a5c8053
MaulanaAdil <kenari222@gmail.com>
2021-11-21 20:51:24 +0700

    configure button elements

---
+++ src/elements/Button/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+import propTypes from 'prop-types';
+
+export default function Button() {
+  return <div></div>;
+}
+
+Button.propTypes = {
+  type: propTypes.oneOf(['button', 'link']),
+  OnClick: propTypes.func,
+  target: propTypes.string,
+  className: propTypes.string,
+  isDisabled: propTypes.bool,
+  isLoading: propTypes.bool,
+  isSmall: propTypes.bool,
+  isLarge: propTypes.bool,
+  isBlock: propTypes.bool,
+  isExternal: propTypes.bool,
+  hasShadow: propTypes.bool,
+};
