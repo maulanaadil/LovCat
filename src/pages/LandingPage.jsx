dfe54b7305b23c5c0c87cf2ec21a2ae64d8e8ae3
MaulanaAdil <kenari222@gmail.com>
2021-11-24 16:38:30 +0700

    Add Hero on Landing Page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 
 import Header from 'parts/Header';
+import Hero from 'parts/Hero';
 export default class LandingPage extends Component {
+  constructor(props) {
+    super(props);
+    this.refMostPicked = React.createRef;
+  }
   render() {
     return (
       <>
         <Header {...this.props} />
+        <Hero refMostPicked={this.refMostPicked} />
       </>
     );
   }
---
+++ src/parts/Hero.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import Fade from 'react-reveal';
+
+import ImageHero from 'assets/images/main-picture.png';
+import Button from 'elements/Button';
+
+export default function Hero(props) {
+  return (
+    <Fade>
+      <section className="container pt-5">
+        <div className="row align-items-center" style={{ marginTop: 20 }}>
+          <div className="col-auto pr-5" style={{ width: 565 }}>
+            <h1
+              className="font-weight-bold line-height-1 mb-3"
+              lineHeight={{ lineHeight: '63px' }}
+            >
+              Find your adorable friend,
+              <br />
+              then happy by adopt it
+            </h1>
+            <p
+              className="font-weight-light text-font-desc w-75 mb-3"
+              style={{ lineHeight: '25px' }}
+            >
+              When you find your friend, you save a loving animal by make them
+              part of your family.
+            </p>
+            <Button
+              className="btn px-5"
+              isPrimary
+              hasShadow
+              type="link"
+              href="/adoption"
+            >
+              Adopt Now
+            </Button>
+          </div>
+          <div className="col-6 pl-5">
+            <img
+              src={ImageHero}
+              alt="pic hero"
+              className="img-fluid image-hero"
+            />
+          </div>
+        </div>
+      </section>
+    </Fade>
+  );
+}
