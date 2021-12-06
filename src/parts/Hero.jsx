403b6e49a17d9e101ced69241258102f4eedeaa4
MaulanaAdil <kenari222@gmail.com>
2021-12-06 17:33:46 +0700

    Add Tilt on hero section

--- src/parts/Hero.jsx
+++ src/parts/Hero.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Fade from 'react-reveal';
+import Tilt from 'react-parallax-tilt';
 
-import ImageHero from 'assets/images/main-picture.png';
+import ImageHero from 'assets/images/image-hero.png';
+import BackgroundHero from 'assets/images/background-vector-hero.png';
+import VectorDot from 'assets/images/vector-dot.png';
 import Button from 'elements/Button';
 
 export default function Hero(props) {
   return (
     <Fade>
-      <section className="container pt-5">
+      <section className="container pt-5" style={{ marginBottom: 80 }}>
         <div className="row align-items-center" style={{ marginTop: 20 }}>
           <div className="col-auto pr-5" style={{ width: 565 }}>
             <h1
@@ -35,12 +38,28 @@ export default function Hero(props) {
               Adopt Now
             </Button>
           </div>
-          <div className="col-6 pl-5">
+          <div className="col-6 pl-5 text-center">
+            <img
+              src={BackgroundHero}
+              alt="background hero"
+              className="img-fluid image-hero-background"
+            />
+            <Tilt
+              className="parallax-effect-glare-scale"
+              perspective={1200}
+              glareEnable={true}
+              glareMaxOpacity={0.45}
+            >
+              <img
+                src={VectorDot}
+                alt="vector dot"
+                className="img-fluid img-vector-dot"
+              />
+            </Tilt>
             <img
               src={ImageHero}
               alt="pic hero"
               className="img-fluid image-hero"
-              style={{ width: 591, height: 'auto' }}
             />
           </div>
         </div>
